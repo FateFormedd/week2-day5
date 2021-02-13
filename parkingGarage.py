b4ba14edf0890e297ab89b0df3bdc8d2515e072b
Anotnio <lateralprogramming@gmail.com>
2021-02-12 18:05:54 -0800

    new commit 6:05

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,41 +1,59 @@
 class Ticket:
-    
+
     def __init__(self):
-        self.tickets = [x + 1 for x in range(10)]
+        self.tickets = []
         self.current = {}
+
     def sell(self):
         x = self.tickets.pop(0)
-        self.current[x] = 'unpaid'
-#         show available spaces
+        self.current[x] = 'Unpaid'
+        return f"Here's your ticket {x}"
 
-    def paid(self):
-        for key, value:
-            key = i
-            value = j
-            self.tickets.append(i)
-            self.spaces.append(j)
-            self.current.del[i] = j
+    def pay(self):
+        x = input("which ticket is yours? ")
+        self.current[x] = 'Paid'
+        return f"You're all paid up!"
 
 
-    def leave():
-        pass
+        # for key, value:
+        #     key = i
+        #     value = j
+        # self.tickets.append(i)
+        # self.spaces.append(j)
+        # self.current.del[i] = j
+        # return
+
+    def leave(self):
+        x = input("What ticket are you returning? ")
+        self.tickets.append(x)
+        del self.current[x]
+        return "Have a great drive!"
+
         
+
 # - takeTicket
 # - This should decrease the amount of tickets available by 1
 # - This should decrease the amount of parkingSpaces available by 1
-# - Update tickets list to increase by 1 (meaning add to the tickets list)        
+# - Update tickets list to increase by 1 (meaning add to the tickets list)
 # class Garage:
 #     def __init__(self):
-       
-class Spaces:
-    def __init__(self):
-        self.spaces = []
+
+# class Spaces:
+#     def __init__(self):
+#         self.spaces = []
+#             return
+
 
 class Test:
-    
-    def main():
+
+    def main(self):
+        user_input = input()
         ticket = Ticket()
         ticket.sell()
-    
-test=Test()
-test.main()
+
+
+test = Test()
+# test.main()
+Ticket()
+myticket = Ticket()
+myticket.pay()
