ae390d91b3fb654f05d84fc1e6e45e277864f915
Anotnio <lateralprogramming@gmail.com>
2021-02-12 19:31:31 -0800

    7:30

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,13 +1,13 @@
 class Ticket:
 
-    def __init__(self):
-        self.tickets = []
+    def __init__(self, tickets):
+        self.tickets = [x + 1 for x in range(10)]
         self.current = {}
 
-    def sell(self):
-        x = self.tickets.pop(0)
-        self.current[x] = 'Unpaid'
-        return f"Here's your ticket {x}"
+    def sell(self, tickes):
+        tickets = self.tickets.pop(0)
+        self.current[tickets] = 'Unpaid'
+        return f"Here's your ticket {tickets}"
 
     def pay(self):
         x = input("which ticket is yours? ")
@@ -23,16 +23,19 @@ class Ticket:
         # self.current.del[i] = j
         # return
 
-    def leave(self):
-        x = input("What ticket are you returning? ")
-        self.tickets.append(x)
-        del self.current[x]
+    def leave(self, tickets):
+        self.tickets = tickets
+        self.tickets.append(self.tickets)
+        del self.current[self.tickets]
         return "Have a great drive!"
 
 
 class Test:
 
     def main(self):
+
+        def leave(self, tickets):
+            self.tickets = input("What ticket are you returning? ")
         user_input = input()
         ticket = Ticket()
         ticket.sell()
